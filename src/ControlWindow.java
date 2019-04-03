454fe7087ac5092264abd7534055675b39fb8efa
Henry Wu <hphwu@uwaterloo.ca>
2019-04-03 15:05:15 -0400

    Minor Fixes

--- src/ControlWindow.java
+++ src/ControlWindow.java
@@ -1,6 +1,8 @@
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 import java.io.*;
 import java.util.Scanner;
 import java.io.*;
@@ -8,7 +10,12 @@ import java.awt.Font;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.JTextField;
+import javax.swing.KeyStroke;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.JButton;
+import javax.swing.JComponent;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
@@ -33,10 +40,11 @@ public class ControlWindow extends JFrame implements KeyListener {
 	static byte pwm;
 	static byte angle;
 
-	// Declare the components;
+	// Declare the components
 	JLabel lblMotorStats;
 	JLabel lblServoStats;
 	JLabel lblPWM;
+	JLabel lblAngle;
 	JTextField lblTxPort;
 	JTextField lblRxPort;
 	JTextField lblCarIP;
@@ -46,7 +54,6 @@ public class ControlWindow extends JFrame implements KeyListener {
 	JButton btnCreateSocket;
 	JSlider PWMSlider;
 	JSlider angleSlider = new JSlider();
-	JLabel lblAngle = new JLabel("30");
 	JLabel DISTANCE_TEXT = new JLabel("Distance: ");
 	static JLabel lblDistance = new JLabel("0");
 	JLabel CM_TEXT = new JLabel("cm");
@@ -69,7 +76,7 @@ public class ControlWindow extends JFrame implements KeyListener {
 	public ControlWindow() {
 		pwm = 30;
 		angle = 30;
-		
+
 		initGUI();
 		addListeners();
 	}
@@ -80,134 +87,145 @@ public class ControlWindow extends JFrame implements KeyListener {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		getContentPane().setLayout(null);
 
-		JLabel lblMotorPwm = new JLabel("Motor PWM:");
-		lblMotorPwm.setBounds(43, 157, 80, 27);
-		getContentPane().add(lblMotorPwm);
+		// ============= Constant texts ===================
+			JLabel MOTOR_PWM_TEXT = new JLabel("Motor PWM:");
+			MOTOR_PWM_TEXT.setBounds(43, 157, 80, 27);
+			getContentPane().add(MOTOR_PWM_TEXT);
+	
+			JLabel SERVO_ANGLE_TEXT = new JLabel("Servo Angle:");
+			SERVO_ANGLE_TEXT.setHorizontalAlignment(SwingConstants.CENTER);
+			SERVO_ANGLE_TEXT.setBounds(159, 157, 94, 27);
+			getContentPane().add(SERVO_ANGLE_TEXT);
+	
+			JLabel CAR_IP_TEXT = new JLabel("Car IP: ");
+			CAR_IP_TEXT.setBounds(20, 60, 44, 16);
+			getContentPane().add(CAR_IP_TEXT);
+	
+			JLabel CAR_PORT_TEXT = new JLabel("Car Port:");
+			CAR_PORT_TEXT.setBounds(228, 60, 61, 16);
+			getContentPane().add(CAR_PORT_TEXT);
+	
+			JLabel TX_PORT_TEXT = new JLabel("Tx Port:");
+			TX_PORT_TEXT.setBounds(20, 27, 80, 16);
+			getContentPane().add(TX_PORT_TEXT);
+	
+			JLabel RX_PORT_TEXT = new JLabel("Rx Port: ");
+			RX_PORT_TEXT.setBounds(228, 27, 61, 16);
+			getContentPane().add(RX_PORT_TEXT);
+	
+			CM_TEXT.setHorizontalAlignment(SwingConstants.CENTER);
+			CM_TEXT.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+			CM_TEXT.setBounds(359, 184, 34, 29);
+			getContentPane().add(CM_TEXT);
+	
+			DISTANCE_TEXT.setBounds(331, 162, 86, 16);
+			getContentPane().add(DISTANCE_TEXT);
 
-		JLabel SERVO_ANGLE_TEXT = new JLabel("Servo Angle:");
-		SERVO_ANGLE_TEXT.setHorizontalAlignment(SwingConstants.CENTER);
-		SERVO_ANGLE_TEXT.setBounds(159, 157, 94, 27);
-		getContentPane().add(SERVO_ANGLE_TEXT);
+		// ============ Mutable components ============
+		// Socket Creation
+			lblCarIP = new JTextField();
+			lblCarIP.setBounds(76, 55, 130, 26);
+			getContentPane().add(lblCarIP);
+			lblCarIP.setColumns(10);
 
-		// Motor Status Label
-		lblMotorStats = new JLabel("0");
-		lblMotorStats.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
-		lblMotorStats.setBounds(74, 173, 49, 40);
-		getContentPane().add(lblMotorStats);
+			lblTxPort = new JTextField();
+			lblTxPort.setBackground(SystemColor.control);
+			lblTxPort.setEditable(true);
+			lblTxPort.setBounds(76, 22, 130, 26);
+			getContentPane().add(lblTxPort);
+			lblTxPort.setColumns(10);
+			
+			
+			lblRxPort = new JTextField();
+			lblRxPort.setBounds(287, 22, 130, 26);
+			getContentPane().add(lblRxPort);
+			lblRxPort.setColumns(10);
 
-		// Servo Status Label
-		lblServoStats = new JLabel("0");
-		lblServoStats.setBounds(200, 173, 44, 40);
-		lblServoStats.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
-		getContentPane().add(lblServoStats);
+			lblCarPort = new JTextField();
+			lblCarPort.setText("80");
+			lblCarPort.setBounds(287, 55, 130, 26);
+			getContentPane().add(lblCarPort);
+			lblCarPort.setColumns(10);
+			
+			btnCreateSocket = new JButton("Create Socket");
+			getContentPane().add(btnCreateSocket);
+			btnCreateSocket.setBounds(159, 88, 143, 29);
+				
+		// Car info
+			lblMotorStats = new JLabel("0");
+			lblMotorStats.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+			lblMotorStats.setBounds(74, 173, 49, 40);
+			getContentPane().add(lblMotorStats);
+	
+			lblServoStats = new JLabel("0");
+			lblServoStats.setBounds(200, 173, 44, 40);
+			lblServoStats.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+			getContentPane().add(lblServoStats);
+			
+			lblDistance.setHorizontalAlignment(SwingConstants.CENTER);
+			lblDistance.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+			lblDistance.setBounds(319, 184, 61, 29);
+			getContentPane().add(lblDistance);
 
-		// User input for socket creation
-		lblCarIP = new JTextField();
-		lblCarIP.setBounds(76, 55, 130, 26);
-		getContentPane().add(lblCarIP);
-		lblCarIP.setColumns(10);
+		// Messages
+			lblMessage1 = new JLabel("");
+			lblMessage1.setForeground(Color.RED);
+			lblMessage1.setHorizontalAlignment(SwingConstants.CENTER);
+			lblMessage1.setFont(new Font("Lucida Grande", Font.PLAIN, 13));
+			lblMessage1.setBounds(43, 129, 160, 16);
+			getContentPane().add(lblMessage1);
+	
+			lblMessage2 = new JLabel("");
+			lblMessage2.setForeground(Color.RED);
+			lblMessage2.setBounds(244, 129, 176, 16);
+			getContentPane().add(lblMessage2);
 
-		lblTxPort = new JTextField();
-		lblTxPort.setBackground(SystemColor.control);
-		lblTxPort.setEditable(true);
-		lblTxPort.setBounds(76, 22, 130, 26);
-		getContentPane().add(lblTxPort);
-		lblTxPort.setColumns(10);
+		// Other things
 
-		lblCarPort = new JTextField();
-		lblCarPort.setText("80");
-		lblCarPort.setBounds(287, 55, 130, 26);
-		getContentPane().add(lblCarPort);
-		lblCarPort.setColumns(10);
-
-		// Constant texts
-		JLabel CAR_IP_TEXT = new JLabel("Car IP: ");
-		CAR_IP_TEXT.setBounds(20, 60, 44, 16);
-		getContentPane().add(CAR_IP_TEXT);
-
-		JLabel CAR_PORT_TEXT = new JLabel("Car Port:");
-		CAR_PORT_TEXT.setBounds(228, 60, 61, 16);
-		getContentPane().add(CAR_PORT_TEXT);
-
-		JLabel TX_PORT_TEXT = new JLabel("Tx Port:");
-		TX_PORT_TEXT.setBounds(20, 27, 80, 16);
-		getContentPane().add(TX_PORT_TEXT);
-
-		// Other stuff
-		lblMessage1 = new JLabel("");
-		lblMessage1.setForeground(Color.RED);
-		lblMessage1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblMessage1.setFont(new Font("Lucida Grande", Font.PLAIN, 13));
-		lblMessage1.setBounds(43, 129, 160, 16);
-		getContentPane().add(lblMessage1);
-
-		lblMessage2 = new JLabel("");
-		lblMessage2.setForeground(Color.RED);
-		lblMessage2.setBounds(244, 129, 176, 16);
-		getContentPane().add(lblMessage2);
-
-		btnCreateSocket = new JButton("Create Socket");
-		getContentPane().add(btnCreateSocket);
-		btnCreateSocket.setBounds(159, 88, 143, 29);
-
-		lblPWM = new JLabel();
-		lblPWM.setHorizontalAlignment(SwingConstants.CENTER);
-		lblPWM.setBounds(56, 225, 44, 16);
-		getContentPane().add(lblPWM);
-
-		PWMSlider = new JSlider();
-		PWMSlider.setMaximum(253);
-		PWMSlider.setValue(30);
-		lblPWM.setText(Integer.toString(PWMSlider.getValue()));
-		PWMSlider.setBounds(13, 199, 124, 29);
-		getContentPane().add(PWMSlider);
-
-		JButton btnFocus = new JButton("Focus");
-		btnFocus.setBounds(179, 243, 117, 29);
-		btnFocus.addKeyListener(this);
-		btnFocus.setFocusable(true);
-		btnFocus.requestFocus();
-		getContentPane().add(btnFocus);
-
-		JLabel RX_PORT_TEXT = new JLabel("Rx Port: ");
-		RX_PORT_TEXT.setBounds(228, 27, 61, 16);
-		getContentPane().add(RX_PORT_TEXT);
-
-		lblRxPort = new JTextField();
-		lblRxPort.setBounds(287, 22, 130, 26);
-		getContentPane().add(lblRxPort);
-		lblRxPort.setColumns(10);
-		
-		angleSlider.setMinimum(5);
-		angleSlider.setMaximum(45);
-		angleSlider.setValue(30);
-		angleSlider.setBounds(140, 199, 124, 29);
-		getContentPane().add(angleSlider);
-
-		lblAngle.setHorizontalAlignment(SwingConstants.CENTER);
-		lblAngle.setBounds(182, 225, 49, 16);
-		getContentPane().add(lblAngle);
-
-		DISTANCE_TEXT.setBounds(331, 162, 86, 16);
-		getContentPane().add(DISTANCE_TEXT);
-
-		lblDistance.setHorizontalAlignment(SwingConstants.CENTER);
-		lblDistance.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
-		lblDistance.setBounds(319, 184, 61, 29);
-		getContentPane().add(lblDistance);
-
-		CM_TEXT.setHorizontalAlignment(SwingConstants.CENTER);
-		CM_TEXT.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
-		CM_TEXT.setBounds(359, 184, 34, 29);
-		getContentPane().add(CM_TEXT);
+			lblPWM = new JLabel();
+			lblPWM.setHorizontalAlignment(SwingConstants.CENTER);
+			lblPWM.setBounds(56, 225, 44, 16);
+			getContentPane().add(lblPWM);
+			
+			lblAngle = new JLabel("30");
+			lblAngle.setHorizontalAlignment(SwingConstants.CENTER);
+			lblAngle.setBounds(182, 225, 49, 16);
+			getContentPane().add(lblAngle);
+	
+			PWMSlider = new JSlider();
+			PWMSlider.setMaximum(253);
+			PWMSlider.setValue(30);
+			lblPWM.setText(Integer.toString(PWMSlider.getValue()));
+			PWMSlider.setBounds(13, 199, 124, 29);
+			PWMSlider.addKeyListener(this);
+			PWMSlider.setFocusable(true);
+			PWMSlider.requestFocus();
+			getContentPane().add(PWMSlider);
+			
+			angleSlider.setMinimum(5);
+			angleSlider.setMaximum(45);
+			angleSlider.setValue(30);
+			angleSlider.setBounds(140, 199, 124, 29);
+			angleSlider.addKeyListener(this);
+			angleSlider.setFocusable(true);
+			angleSlider.requestFocus();
+			getContentPane().add(angleSlider);
+	
+			JButton btnFocus = new JButton("Focus");
+			btnFocus.setBounds(179, 243, 117, 29);
+			btnFocus.addKeyListener(this);
+			btnFocus.setFocusable(true);
+			btnFocus.requestFocus();
+			getContentPane().add(btnFocus);
 	}
 
 	private void addListeners() {
-		btnCreateSocket.addActionListener(new ActionListener() { // Create button is pressed, try to create sender and receiver
-			
+
+		btnCreateSocket.addActionListener(new ActionListener() { // Create button is pressed, try to create sender and
+																	// receiver
 			public void actionPerformed(ActionEvent e) {
 				try {
-					listener.listeningSocket.close();		// Try to close existing socket so we don't get binding error
+					listener.listeningSocket.close(); // Try to close existing socket so we don't get binding error
 					sender.sendingSocket.close();
 				} catch (Exception e3) {
 					System.out.println("Closing error, its ok");
@@ -216,7 +234,7 @@ public class ControlWindow extends JFrame implements KeyListener {
 				try {
 					// First randomize a Tx port
 					lblTxPort.setText(Integer.toString(ThreadLocalRandom.current().nextInt(20000, 30000)));
-					
+
 					// Create sender and receiver
 					sender = new UDPSender(Integer.parseInt(lblTxPort.getText()), lblCarIP.getText(),
 							Integer.parseInt(lblCarPort.getText()));
@@ -237,22 +255,22 @@ public class ControlWindow extends JFrame implements KeyListener {
 				btnCreateSocket.setText("Recreate Socket");
 			}
 		});
-		
-		angleSlider.addChangeListener(new ChangeListener() {	// Whenever a change is detected on the slider
+
+		angleSlider.addChangeListener(new ChangeListener() { // Whenever a change is detected on the slider
 			public void stateChanged(ChangeEvent e) {
 				lblAngle.setText(Integer.toString(angleSlider.getValue()));
-				angle = (byte) angleSlider.getValue();		// Store new angle into angle
+				angle = (byte) angleSlider.getValue(); // Store new angle into angle
 			}
 		});
-		
-		PWMSlider.addChangeListener(new ChangeListener() {		// Similar as above
+
+		PWMSlider.addChangeListener(new ChangeListener() { // Similar as above
 			public void stateChanged(ChangeEvent e) {
 				lblPWM.setText(Integer.toString(PWMSlider.getValue()));
 				pwm = (byte) PWMSlider.getValue();
 			}
 		});
 	}
-	
+
 	@Override
 	public void keyPressed(KeyEvent e) {
 		switch ((char) e.getKeyCode()) {
@@ -292,7 +310,8 @@ public class ControlWindow extends JFrame implements KeyListener {
 			break;
 		}
 	}
-	
+
 	@Override
-	public void keyTyped(KeyEvent e) {}
+	public void keyTyped(KeyEvent e) {
+	}
 }
